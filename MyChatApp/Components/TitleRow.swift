6393ca6182f8453e787b246310ebf405c6987697
alejandropascuale <apascuale@outlook.com>
2022-02-24 12:09:33 -0300

    fill titleRow component with sample data and add style

--- MyChatApp/Components/TitleRow.swift
+++ MyChatApp/Components/TitleRow.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TitleRow: View {
     var imageUrl = URL(string: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80")
-    var name = "Sarah Smith"
+    var name = "Alejandro Pascuale"
     
     var body: some View {
         HStack(spacing: 20) {
@@ -21,12 +21,30 @@ struct TitleRow: View {
             } placeholder: {
                 ProgressView()
             }
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.title).bold()
+                    .foregroundColor(.white)
+                Text("Online")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "phone.fill")
+                .foregroundColor(.gray)
+                .padding(10)
+                .background(.white)
+                .cornerRadius(50)
         }
+        .padding()
     }
 }
 
 struct TitleRow_Previews: PreviewProvider {
     static var previews: some View {
         TitleRow()
+            .background(Color("Main"))
     }
 }
