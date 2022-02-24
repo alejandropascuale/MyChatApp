0f4835ce505c3c69633dbdb47a85415e61498a44
alejandropascuale <apascuale@outlook.com>
2022-02-24 10:05:26 -0300

    Initial Commit

---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by Alejandro Pascuale on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
