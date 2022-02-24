6dbc4c9b520c17cebe0206716471a86560bcf200
alejandropascuale <apascuale@outlook.com>
2022-02-24 10:26:50 -0300

    create titleRow component and add profile image

---
+++ MyChatApp/Components/TitleRow.swift
@@ -0,0 +1,32 @@
+//
+//  TitleRow.swift
+//  MyChatApp
+//
+//  Created by Alejandro Pascuale on 24/02/2022.
+//
+
+import SwiftUI
+
+struct TitleRow: View {
+    var imageUrl = URL(string: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80")
+    var name = "Sarah Smith"
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            AsyncImage(url: imageUrl) {
+                image in image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(50)
+            } placeholder: {
+                ProgressView()
+            }
+        }
+    }
+}
+
+struct TitleRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleRow()
+    }
+}
