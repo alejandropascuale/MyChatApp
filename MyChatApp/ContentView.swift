ff7e2f97c482330419a01eaa05e86d62ccf84239
alejandropascuale <apascuale@outlook.com>
2022-02-24 16:11:41 -0300

    use extension in ContentView to new shape in messages bubble

--- MyChatApp/ContentView.swift
+++ MyChatApp/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
             }
             .padding(.top, 10)
             .background(.white)
+            .cornerRadius(30, corners: [.topLeft, .topRight])
         }
         .background(Color("Main"))
     }
